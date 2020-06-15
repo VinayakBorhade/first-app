414b10c5ecc3784c81dbe4e5578bca5d3e8e9ea4
VinayakBorhade <vinayakborhade@gmail.com>
2020-06-15 14:08:39 +0530

    property binding with directive properties

---
+++ src/app/databinding/databinding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fa-databinding',
+  templateUrl: './databinding.component.html',
+  styleUrls: ['./databinding.component.css']
+})
+export class DatabindingComponent {
+  s = "String interpolation";
+
+  onTest(){
+    return true;
+  }
+}
